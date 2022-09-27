4e139b0a324f08a20d3a74ed793071112f415aca
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-09-27 11:29:47 +0300

    Done.

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,18 @@
+class BirdsController < ApplicationController
+
+    def index
+        birds = Bird.all
+        render json: birds
+        # render json: Bird.all
+    end
+
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: { message: 'Bird not found' }, status: :not_found
+        end
+    end
+
+end
